7e338dcbce01b6342b88612da04c1c5d5c6cf86a
Ozgur Vatansever <ozgur.vatansever@betbull.com>
2021-05-05 19:16:44 +0300

    added carthage support

---
+++ LeanplumSegment/Resources/LeanplumSegment.h
@@ -0,0 +1,18 @@
+//
+//  LeanplumSegment.h
+//  LeanplumSegment
+//
+//  Created by Ozgur Vatansever on 5.05.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LeanplumSegment.
+FOUNDATION_EXPORT double LeanplumSegmentVersionNumber;
+
+//! Project version string for LeanplumSegment.
+FOUNDATION_EXPORT const unsigned char LeanplumSegmentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like
+#import <LeanplumSegment/SEGLeanplumIntegration.h>
+#import <LeanplumSegment/SEGLeanplumIntegrationFactory.h>
